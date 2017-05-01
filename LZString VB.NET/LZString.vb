ff13be81ee0c1ed5cb2c9517739d8259a89b89a5
gsemac <gabe.semac@gmail.com>
2017-05-01 10:27:16 -0600

    Added project files and unit testing project

---
+++ LZString VB.NET/LZString.vb
@@ -0,0 +1,5 @@
+﻿Public Class LZString
+
+
+
+End Class
---
+++ LZString VB.NET/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LZString VB.NET")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Toshiba")> 
+<Assembly: AssemblyProduct("LZString VB.NET")> 
+<Assembly: AssemblyCopyright("Copyright © Toshiba 2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7466f505-00bf-4803-8203-5998d93ee6f8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
